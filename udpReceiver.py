bf9706393eeb16596caed1887fe25b9d6f3b1582
Christian Fisher <christian.fisher@carleton.ca>
2019-09-20 20:23:59 -0400

    AAkash's version

--- udpReceiver.py
+++ udpReceiver.py
@@ -3,7 +3,6 @@
 import socket, sys, time
 
 textport = sys.argv[1]
-host = sys.argv[2]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
@@ -13,29 +12,10 @@ s.bind(server_address)
 while True:
 
     print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+
     buf, address = s.recvfrom(port)
-    
     if not len(buf):
         break
     print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
-    print("ACK: %s" % buf)
 
-    #if buf:
-    #    messageSent = s.sendto(buf, address)
-    #    print("sent %s bytes back to %s" % (messageSent, address))
-
-
-    print ("Enter data to reply: ENTER to quit")
-    datareceive = sys.stdin.readline().strip()
-
-        #s.sendall(data.encode('utf-8'))
-    #Sending response
-    print("Sending %s" % datareceive)
-    s.sendto(datareceive.encode('utf-8'), server_address)
-        
-
-    # Receiving response
-    print("Waiting to receive response")
-    sender=s.recvfrom(port)
-    print(" Response received %s" % datareceive)
 s.shutdown(1)
--- udpSender.py
+++ udpSender.py
@@ -2,38 +2,20 @@
 
 import socket, sys, time
 
-
 host = sys.argv[1]
 textport = sys.argv[2]
-number = int(sys.argv[3])
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
-increment = 0
+
 while 1:
     print ("Enter data to transmit: ENTER to quit")
     data = sys.stdin.readline().strip()
-    
     if not len(data):
         break
-    while(increment < number):
-        increment = increment + 1
-        data+= str(increment)
-        
-
-        #s.sendall(data.encode('utf-8'))
-        #Sending response
-        print("Sending %s" % data)
-        s.sendto(data.encode('utf-8'), server_address)
-        data = data[:-1]
-        
-
-        # Receiving response
-        print("Waiting to receive response")
-        sender=s.recvfrom(port)
-        print(" Response received %s" % data)
+#    s.sendall(data.encode('utf-8'))
+    s.sendto(data.encode('utf-8'), server_address)
 
 s.shutdown(1)
 
